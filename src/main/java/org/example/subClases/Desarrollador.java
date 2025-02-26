c439f2b9c1bec51cbf832ea0731108262c1d6114
July924 <marielaventura924@gmail.com>
2025-02-26 13:09:51 -0600

    Creacion de las pruebas initarias

--- src/main/java/org/example/subClases/Desarrollador.java
+++ src/main/java/org/example/subClases/Desarrollador.java
@@ -4,14 +4,8 @@ import org.example.clasesPrincipales.Empleado;
 
 // Subclases especializadas
 public class Desarrollador extends Empleado {
-    private String proyecto;
-
     public Desarrollador(String nombre, int id, double salario, String proyecto) {
         super(nombre, id, salario, "Desarrollo de Software");
-        this.proyecto = proyecto;
     }
 
-    public boolean tieneProyecto() {
-        return proyecto != null && !proyecto.isEmpty();
-    }
 }
--- src/main/java/org/example/subClases/Gerente.java
+++ src/main/java/org/example/subClases/Gerente.java
@@ -3,6 +3,7 @@ package org.example.subClases;
 import org.example.clasesPrincipales.Empleado;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Gerente extends Empleado {
@@ -19,5 +20,9 @@ public class Gerente extends Empleado {
 
     public int getTamanoEquipo() {
         return equipo.size();
+    }
+
+    public Collection<Object> getDesarrolladores() {
+        return List.of();
     }
 }
